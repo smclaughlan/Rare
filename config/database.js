bc3d4b1e6ac63e3209499d2a4e6cc2f2391ca141
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-02-26 12:24:10 -0800

    Remove ssl from db config

--- config/database.js
+++ config/database.js
@@ -19,9 +19,9 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    "dialectOptions": {
-      "ssl": true
-    }
+    // "dialectOptions": {
+    //   "ssl": true
+    // }
   },
   test: {
     dialect: "sqlite",
